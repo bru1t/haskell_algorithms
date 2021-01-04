07f9308318349372eb5a46d3d85d218fa3426d2c
Alexey Kuznetsov <41516103+bru1t@users.noreply.github.com>
2021-01-04 20:24:44 +0300

    Update 01/04/21 - 20:24

--- main_files/nums_algorithms.hs
+++ main_files/nums_algorithms.hs
@@ -12,3 +12,13 @@ signum' x
         | otherwise = 0
 
 -- Number Transformation -----------------------------------------------------<
+
+-- Solving a quadratic equation in format: (a,b,c) -> a*x^2+b*x+c=0
+quadeq :: (Float, Float, Float) -> [Float]
+quadeq (0,b,c) = [ (-c) / b]
+quadeq (a,b,c)
+        | d == 0 = [ root (+) ]
+        | otherwise = [ root (+) , root (-) ]
+               where
+                       d = b*b-4*a*c
+                       root sign = sign (-b) (sqrt d) / (2*a)
